4eec1d4bc4095928520bd57f7d1dbd260a2d8f54
LeosPunch <114724732+LeosPunch@users.noreply.github.com>
2026-03-07 16:43:54 +0300

    Add files via upload

---
+++ maps.js
@@ -0,0 +1,46 @@
+const mapData = {
+
+ditchfest: {
+title: "Ditchfest",
+image: "https://download.dashmap.live/3b6a6355-5da4-4fe7-85fe-0ee7a5c4c138/large.png",
+maps: [
+{ tmx:287893, uid:"i7fiiSzZIlxdjoVaQdcdoKfBwTa", name:"DF114 - Restricted Pool" },
+{ tmx:297260, uid:"fUwsGXyoEX4lekHMokalenVj034", name:"DF117 - Straight PF map" },
+{ tmx:293767, uid:"Z6907M4ga_LSkTq0RuVsSbTrjG3", name:"DF116 - Затонувший Курорт Богов Скорости" },
+{ tmx:280725, uid:"KeAc4n63BNGSxO6h4ut0Kd7N_wb", name:"DF111 SlideScroller" },
+{ tmx:283154, uid:"oOSCyjyPCSdVspORqTOOJ8iLzId", name:"DF112 - ENGINE REDy" },
+{ tmx:285882, uid:"JZeIYjg7zkD3XIu_aRtev8Pvr9a", name:"DF113 - R U WANNA BUILD A SNOWMAN" },
+{ tmx:290462, uid:"oWaCGJuV6O19bWiQmAdvTcQQlc9", name:"DF115 My First Map" },
+{ tmx:285881, uid:"9q6kdND57eiSbahVc1bLWf_rx9g", name:"DF113 - Dress The Tree" },
+{ tmx:300579, uid:"qIfIUBeM21nyGbY9VUkkHJaAr5", name:"DF 118 - DF118" },
+{ tmx:300580, uid:"YQMmgF5GEJWApBZnvAeYk6bqS14", name:"DF118 - 3 - NadeoStyle PuzzleMapping" },
+{ tmx:300581, uid:"FA6jNzldki68dRGWdLMHddXDyN6", name:"DF 118 - Grand Enduro" },
+{ tmx:300582, uid:"TmienlQNmzataanQ9M_4fOZ0_jk", name:"DF118 allmaps allcars allteach allditch" }
+]
+},
+
+ws: {
+title: "Weekly Shorts",
+image: "https://download.dashmap.live/3b6a6355-5da4-4fe7-85fe-0ee7a5c4c138/ws.png",
+maps: [
+{ tmx:280708, uid:"jxoqsle8GrVnzuAuPizAd69Hrn5", name:"2 - Castle Simulation" },
+{ tmx:280714, uid:"DSwL0QwrWwrhYzyGBqnYUJrcok4", name:"2 - Castle's Pass" },
+{ tmx:280715, uid:"Dd7mo2_zCVRRMJ0jEPP6a3FS5_c", name:"1 - Save Our Cows" },
+{ tmx:280718, uid:"YR4eImvTmLNPHA38xDwIoANsdz3", name:"2 - Foggy Cycling (ft Baogab)" },
+{ tmx:280719, uid:"xfVP8s75q1eLRX5vL2wDQGhyUUd", name:"1 - SPOOKY PUMPKINS" },
+{ tmx:280721, uid:"_HYKs2jNHoxxZ2IotnFfPco0Bwe", name:"4 - Lime Matcha" }
+]
+},
+
+mwms: {
+title: "Mowam Shorts",
+image: "https://download.dashmap.live/3b6a6355-5da4-4fe7-85fe-0ee7a5c4c138/mwms.png",
+maps: [
+{ tmx:300752, uid:"Dezkr5nDL2R_0lEkMbUZvGXjf3m", name:"Winter Village" },
+{ tmx:300755, uid:"uSljTMy7Bt4u389_PlhxO0me6sa", name:"hidden island" },
+{ tmx:300756, uid:"JawN8bAjxJarDej34HX2IuYa9B1", name:"map skipped to prevent server crashing" },
+{ tmx:300757, uid:"JA0G9yWZaGbl8yqb1WRAFBUztek", name:"Under Construction" }
+]
+}
+
+};
